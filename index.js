b2e69e8b9f7f0dec3cca3bfac869fe72e1f7faee
LanceStemple <stemplel@msu.edu>
2023-11-22 15:10:09 -0500

    Added proper randomizer pool for each sort setting

--- index.js
+++ index.js
@@ -29,19 +29,48 @@ function randomizeChamp(sort){
                 "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"];
             break;
         case 'mid':
-
+                champNames = ["Ahri", "Akali", "Akshan", "Anivia", "Annie",
+                "Aurelion Sol", "Azir", "Cassiopeia", "Corki", "Ekko", "Fizz", "Galio",
+                "Irelia", "Jayce", "Kassadin", "Katarina", "LeBlanc", "Lissandra", "Lux", "Malzahar", "Naafiri", "Neeko",
+                "Orianna", "Pantheon", "Qiyana", "Ryze",
+                "Sylas", "Syndra", "Taliyah", "Talon", "Tristana", "Twisted Fate",
+                "Veigar", "Vex", "Viktor", "Vladimir", "Xerath", "Yasuo", "Yone",
+                "Zed", "Ziggs", "Zoe"];
             break;
         case 'top':
-
+            champNames = ["Aatrox", "Akali", "Camille", "Cho'Gath", "Darius", "Dr. Mundo", "Fiora",
+                "Gangplank", "Garen", "Gnar", "Gwen", "Illaoi",
+                "Irelia", "Jax", "Jayce", "Kayle", "Kennen",
+                "Kled", "K'Sante", "Malphite",
+                "Mordekaiser", "Nasus", "Olaf", "Ornn", "Pantheon", "Quinn", "Renekton",
+                "Riven", "Rumble", "Sett", "Shen", "Singed", "Sion", "Tahm Kench", "Teemo",
+                "Trundle", "Tryndamere", "Urgot", "Volibear", "Yasuo", "Yone", "Yorick"];
             break;
         case 'jungle':
-
+            champNames = ["Amumu", "Bel'Veth",
+                "Briar", "Diana", "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Gragas", "Graves", "Hecarim", "Ivern", "Jarvan IV", "Karthus", "Kayn", "Kha'Zix", "Kindred",
+                "Lee Sin", "Lillia", "Maokai", "Master Yi", "Nidalee",
+                "Nocturne", "Nunu & Willump", "Poppy", "Rammus", "Rek'Sai",
+                "Rengar", "Sejuani",
+                "Shaco", "Shyvana", "Skarner",
+                "Sylas", "Taliyah",
+                "Trundle", "Udyr", "Vi", "Viego", "Warwick",
+                "Wukong", "Xin Zhao", "Zac"];
             break;
         case 'bottom':
-
+            champNames = ["Aphelios", "Ashe", "Caitlyn",
+                "Draven", "Ezreal", "Jhin", "Jinx", "Kai'Sa", "Kalista",
+                "Kog'Maw", "Lucian", "Miss Fortune",
+                "Nilah", "Samira", "Sivir", "Tristana", "Twitch", "Varus", "Vayne",
+                "Xayah", "Yasuo", "Zeri", "Ziggs"];
             break;
         case 'support':
-
+            champNames = ["Alistar", "Ashe", "Bard", "Blitzcrank", "Brand", "Braum", "Heimerdinger", "Janna",
+                "Karma", "Leona",
+                "Lulu", "Lux", "Milio", "Morgana", "Nami", "Nautilus", "Neeko", "Pantheon",
+                "Pyke", "Rakan", "Rell", "Renata Glasc", "Senna", "Seraphine",
+                "Shaco", "Sona", "Soraka", "Swain",
+                "Taric", "Thresh", "Vel'Koz", "Xerath", "Yuumi", "Zilean", "Zyra"];
             break;
     }
 
